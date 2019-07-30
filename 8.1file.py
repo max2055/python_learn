31743c1ef519a1ef79f1248099cbe7cc4bfc0cd7
unknown <wucl@tpam.local>
2019-07-30 17:06:54 +0800

    fileOperation

---
+++ 8.1file.py
@@ -0,0 +1,26 @@
+import os
+# # path 
+# print(os.path.join('c:\\', 'Windows', 'etc'))
+# print(os.getcwd())
+# # print(os.chdir('d:\\'))
+# # print(os.getcwd())
+# # os.makedirs('D:\\Users\\wucl\\Desktop\\Test\\t1')
+# print(os.path.abspath('.'))
+# print(os.path.isabs('.'))
+# print(os.path.isabs(os.path.abspath('.')))
+# print(
+#     os.path.relpath('D:\\Users\\wucl\\Desktop\\Test\\t1'))
+# print(os.path.dirname('D:\\Users\\wucl\\Desktop\\Test\\t1'))
+# print(os.path.basename('D:\\Users\\wucl\\Desktop\\Test\\t1'))
+# print(os.path.split('D:\\Users\\wucl\\Desktop\\Test\\t1'))
+
+# operation
+helloFile=open(os.path.join(os.getcwd(),'python_learn','hello.txt'),'r')
+print(helloFile.readlines())
+helloFile.close()
+helloFile=open(os.path.join(os.getcwd(),'python_learn','hello.txt'),'a')
+helloFile.write('\nyou are welcome')
+helloFile.close()
+helloFile=open(os.path.join(os.getcwd(),'python_learn','hello.txt'),'r')
+print(helloFile.readlines())
+helloFile.close()
