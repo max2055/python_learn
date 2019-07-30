685e1b6f8789c43395d002ef8522db4b0255a579
unknown <wucl@tpam.local>
2019-07-30 10:37:07 +0800

    phoneAndEmail

--- 7.15-phoneAndEmail.py
+++ 7.15-phoneAndEmail.py
@@ -1,29 +1,43 @@
-import pyperclip, re
-text = pyperclip.paste()
-phoneRegex = re.compile(r'''
-        (\(\+86\))?
-        (\s)?
-        (\d{3})
-        (\s|-|\.)?
-        (\d{4})
-        (\s|-|\.)?
-        (\d{4})
-        ''', re.VERBOSE)
+#! python3
+# phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
 
+import pyperclip, re
+
+# Create phone regex.
+phoneRegex = re.compile(
+    r'''(
+    (\d{3}|\(\d{3}\))? # area code
+    (\s|-|\.)? # separator
+    (\d{3}) # first 3 digits
+    (\s|-|\.) # separator
+    (\d{4}) # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+    )''', re.VERBOSE)
+# Create email regex.
+emailRegex = re.compile(
+    r'''(
+    [a-zA-Z0-9._%+-]+ # username
+    @ # @ symbol
+    [a-zA-Z0-9.-]+ # domain name
+    (\.[a-zA-Z]{2,4}) # dot-something
+    )''', re.VERBOSE)
+# TODO: Find matches in clipboard text.
+text = str(pyperclip.paste())
 matches = []
+# result=phoneRegex.findall(text)
+# print(result)
 for groups in phoneRegex.findall(text):
-        phoneNum = '-'.join([groups[2], groups[4], groups[6]])
-        matches.append(phoneNum)
-'''
-text:
-(+86) 18117132051
-18117132052
-181 1713 2053
-181-1713-2054
-'''
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# TODO: Copy results to the clipboard.
 if len(matches) > 0:
-        pyperclip.copy('\n'.join(matches))
-        print('Copied to clipboard:')
-        print('\n'.join(matches))
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
 else:
-        print('No phone numbers or email addresses found.')
+    print('No phone numbers or email addresses found.')
---
+++ pickCNphoneNumber.py
@@ -0,0 +1,32 @@
+'''
+text:
+(+86) 18117132051
+18117132052
+181 1713 2053
+181-1713-2054
+'''
+
+import pyperclip, re
+text = pyperclip.paste()
+phoneRegex = re.compile(
+    r'''
+        (\(\+86\))?
+        (\s)?
+        (\d{3})
+        (\s|-|\.)?
+        (\d{4})
+        (\s|-|\.)?
+        (\d{4})
+        ''', re.VERBOSE)
+
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[2], groups[4], groups[6]])
+    matches.append(phoneNum)
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
