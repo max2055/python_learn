f8e4ea5af89846995f10a5c48a991cd54f4bba82
unknown <wucl@tpam.local>
2019-07-30 17:07:10 +0800

    fileOperation

--- 8.1file.py
+++ 8.1file.py
@@ -1,5 +1,5 @@
 import os
-# # path 
+# # path
 # print(os.path.join('c:\\', 'Windows', 'etc'))
 # print(os.getcwd())
 # # print(os.chdir('d:\\'))
@@ -15,12 +15,12 @@ import os
 # print(os.path.split('D:\\Users\\wucl\\Desktop\\Test\\t1'))
 
 # operation
-helloFile=open(os.path.join(os.getcwd(),'python_learn','hello.txt'),'r')
+helloFile = open(os.path.join(os.getcwd(), 'python_learn', 'hello.txt'), 'r')
 print(helloFile.readlines())
 helloFile.close()
-helloFile=open(os.path.join(os.getcwd(),'python_learn','hello.txt'),'a')
+helloFile = open(os.path.join(os.getcwd(), 'python_learn', 'hello.txt'), 'a')
 helloFile.write('\nyou are welcome')
 helloFile.close()
-helloFile=open(os.path.join(os.getcwd(),'python_learn','hello.txt'),'r')
+helloFile = open(os.path.join(os.getcwd(), 'python_learn', 'hello.txt'), 'r')
 print(helloFile.readlines())
 helloFile.close()
